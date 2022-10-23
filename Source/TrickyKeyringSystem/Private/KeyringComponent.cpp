81c8cd9522821b1bb9684b50200986747c52802a
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-23 21:48:35 +0300

    Remove unnecessary functions

--- Source/TrickyKeyringSystem/Private/KeyringComponent.cpp
+++ Source/TrickyKeyringSystem/Private/KeyringComponent.cpp
@@ -3,32 +3,7 @@
 
 #include "KeyringComponent.h"
 
-// Sets default values for this component's properties
 UKeyringComponent::UKeyringComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	PrimaryComponentTick.bCanEverTick = false;
 }
-
-
-// Called when the game starts
-void UKeyringComponent::BeginPlay()
-{
-	Super::BeginPlay();
-
-	// ...
-	
-}
-
-
-// Called every frame
-void UKeyringComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
-}
-
--- Source/TrickyKeyringSystem/Public/KeyringComponent.h
+++ Source/TrickyKeyringSystem/Public/KeyringComponent.h
@@ -13,16 +13,6 @@ class TRICKYKEYRINGSYSTEM_API UKeyringComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UKeyringComponent();
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
 };
