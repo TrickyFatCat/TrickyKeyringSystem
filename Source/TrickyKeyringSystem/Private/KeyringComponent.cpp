732536234e935ddb548f724a6b32f1225b388cd7
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-23 22:41:02 +0300

    Implement basic logic

--- Source/TrickyKeyringSystem/Private/KeyringComponent.cpp
+++ Source/TrickyKeyringSystem/Private/KeyringComponent.cpp
@@ -7,3 +7,149 @@ UKeyringComponent::UKeyringComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
 }
+
+bool UKeyringComponent::AddKey(TSubclassOf<UKeyType> KeyType)
+{
+	if (!IsValid(KeyType))
+	{
+		return false;
+	}
+
+	if (HasKey(KeyType))
+	{
+		return false;
+	}
+
+	UKeyType* NewKey = NewObject<UKeyType>(this, *KeyType);
+	Keyring.Emplace(NewKey);
+	OnKeyAdded.Broadcast(NewKey);
+	
+	return true;
+}
+
+bool UKeyringComponent::RemoveKey(const TSubclassOf<UKeyType> KeyType)
+{
+	if (!IsValid(KeyType))
+	{
+		return false;
+	}
+
+	if (!HasKey(KeyType))
+	{
+		return false;
+	}
+
+	UKeyType* Key = GetKey(KeyType);
+
+	if (!IsValid(Key))
+	{
+		return false;
+	}
+
+	Keyring.Remove(Key);
+	OnKeyRemoved.Broadcast(Key);
+	Key->ConditionalBeginDestroy();
+	
+	return true;
+}
+
+bool UKeyringComponent::UseKey(TSubclassOf<UKeyType> KeyType)
+{
+	if (!IsValid(KeyType))
+	{
+		return false;
+	}
+
+	if (!HasKey(KeyType))
+	{
+		return false;
+	}
+
+	UKeyType* Key = GetKey(KeyType);
+
+	if (!IsValid(Key))
+	{
+		return false;
+	}
+
+	OnKeyUsed.Broadcast(Key);
+
+	if (Key->GetDestroyOnUse())
+	{
+		RemoveKey(KeyType);
+	}
+
+	return true;
+}
+
+bool UKeyringComponent::KeyringIsEmpty() const
+{
+	return Keyring.Num() == 0;
+}
+
+bool UKeyringComponent::HasKey(const TSubclassOf<UKeyType> KeyType)
+{
+	if (!IsValid(KeyType))
+	{
+		return false;
+	}
+
+	auto Predicate = [&](const UKeyType* Key) { return Key->IsA(KeyType); };
+	return Keyring.ContainsByPredicate(Predicate);
+}
+
+void UKeyringComponent::GetKeyDataByIndex(const int32 Index, FKeyData& KeyData)
+{
+	if (KeyringIsEmpty())
+	{
+		return;
+	}
+
+	if (Index < 0 || Index >= Keyring.Num())
+	{
+		return;
+	}
+
+	UKeyType* Key = Keyring[Index];
+
+	if (!IsValid(Key))
+	{
+		return;
+	}
+	
+	KeyData = Key->GetKeyData();
+}
+
+void UKeyringComponent::GetKeyDataByClass(const TSubclassOf<UKeyType> KeyType, FKeyData& KeyData)
+{
+	if (KeyringIsEmpty())
+	{
+		return;
+	}
+
+	if (!HasKey(KeyType) || !IsValid(KeyType))
+	{
+		return;
+	}
+
+	auto Predicate = [&](const UKeyType* Key) { return Key->IsA(KeyType); };
+	const UKeyType* Key = *Keyring.FindByPredicate(Predicate);
+
+	if (!IsValid(Key))
+	{
+		return;
+	}
+	
+	KeyData = Key->GetKeyData();
+}
+
+UKeyType* UKeyringComponent::GetKey(const TSubclassOf<UKeyType> KeyType)
+{
+	if (!IsValid(KeyType))
+	{
+		return nullptr;
+	}
+	
+	auto Predicate = [&](const UKeyType* Key) { return Key->IsA(KeyType); };
+	return *Keyring.FindByPredicate(Predicate);
+}
--- Source/TrickyKeyringSystem/Public/KeyringComponent.h
+++ Source/TrickyKeyringSystem/Public/KeyringComponent.h
@@ -3,16 +3,57 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "KeyType.h"
 #include "Components/ActorComponent.h"
 #include "KeyringComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnKeyAddedSignature, UKeyType*, Key);
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnKeyUsedSignature, UKeyType*, Key);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnKeyRemovedSignature, UKeyType*, Key);
+
+UCLASS(ClassGroup=(TrickyPrototyping), meta=(BlueprintSpawnableComponent))
 class TRICKYKEYRINGSYSTEM_API UKeyringComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	UKeyringComponent();
 
+	UPROPERTY(BlueprintAssignable, Category="KeyringSystem")
+	FOnKeyAddedSignature OnKeyAdded;
+
+	UPROPERTY(BlueprintAssignable, Category="KeyringSystem")
+	FOnKeyUsedSignature OnKeyUsed;
+
+	UPROPERTY(BlueprintAssignable, Category="KeyringSystem")
+	FOnKeyRemovedSignature OnKeyRemoved;
+
+	UFUNCTION(BlueprintCallable, Category="KeyringSystem")
+	bool AddKey(TSubclassOf<UKeyType> KeyType);
+
+	UFUNCTION(BlueprintCallable, Category="KeyringSystem")
+	bool RemoveKey(TSubclassOf<UKeyType> KeyType);
+
+	UFUNCTION(BlueprintCallable, Category="KeyringSystem")
+	bool UseKey(TSubclassOf<UKeyType> KeyType);
+
+	UFUNCTION(BlueprintCallable, Category="KeyringSystem")
+	bool KeyringIsEmpty() const;
+
+	UFUNCTION(BlueprintCallable, Category="KeyringSystem")
+	bool HasKey(TSubclassOf<UKeyType> KeyType);
+
+	UFUNCTION(BlueprintCallable, Category="KeyringSystem")
+	void GetKeyDataByIndex(const int32 Index, FKeyData& KeyData);
+
+	UFUNCTION(BlueprintCallable, Category="KeyringSystem")
+	void GetKeyDataByClass(const TSubclassOf<UKeyType> KeyType, FKeyData& KeyData);
+
+	UKeyType* GetKey(const TSubclassOf<UKeyType> KeyType);
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Keyring", meta=(AllowPrivateAccess))
+	TArray<UKeyType*> Keyring;
 };
