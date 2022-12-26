c26d42cfdc4b10561d3621c7dca478f4c2bdd8d9
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-12-26 19:26:30 +0200

    Add comments

--- Source/TrickyKeyringSystem/Public/LockComponent.h
+++ Source/TrickyKeyringSystem/Public/LockComponent.h
@@ -24,21 +24,27 @@ class TRICKYKEYRINGSYSTEM_API ULockComponent : public UActorComponent
 public:
 	ULockComponent();
 
+	/** Called then the component was locked. */
 	UPROPERTY(BlueprintAssignable)
 	FOnLockedSignature OnLocked;
 
+	/** Called then the component was unlocked. */
 	UPROPERTY(BlueprintAssignable)
 	FOnUnlockedSignature OnUnlocked;
 
+	/** Called then the component can't be locked. */
 	UPROPERTY(BlueprintAssignable)
 	FOnCantLockSignature OnCantLocked;
 
+	/** Called then the component can't be unlocked. */
 	UPROPERTY(BlueprintAssignable)
 	FOnCantUnlockSignature OnCantUnlocked;
 
+	/** Unlocks the component. */
 	UFUNCTION(BlueprintCallable, Category="LockComponent")
 	bool Lock(AActor* TargetActor);
 
+	/** Locks the component. */
 	UFUNCTION(BlueprintCallable, Category="LockComponent")
 	bool Unlock(AActor* TargetActor);
 
@@ -48,18 +54,22 @@ public:
 	UFUNCTION(BlueprintSetter, Category="LockComponent")
 	void SetIsLocked(bool Value);
 
+	/**Checks if the component can be unlocked. */
 	UFUNCTION(BlueprintSetter, Category="LockComponent")
 	bool CanUnlock(const AActor* TargetActor) const;
 
 protected:
 	virtual void BeginPlay() override;
 
+	/** Key type which is required to unlock the component. */
 	UPROPERTY(EditAnywhere, Category="LockSettings", meta=(AllowPrivateAccess=true))
 	TSubclassOf<UKeyType> RequiredKey = nullptr;
 
+	/** Toggles if the key required to lock the component. */
 	UPROPERTY(EditAnywhere, Category="LockSettings", meta=(AllowPrivateAccess=true))
 	bool bLockRequiresKey = true;
 
+	/** Determines if the component is locked or not. */
 	UPROPERTY(EditAnywhere, BlueprintGetter=GetIsLocked, BlueprintSetter=SetIsLocked, Category="LockSettings")
 	bool bIsLocked = true;
 
